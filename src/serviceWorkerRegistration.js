8b28594dd9eef6ae209d6981d03aa5c18267071d
Carlos Quiroz <77166972+carlosq-ordering@users.noreply.github.com>
2024-07-18 15:59:42 -0400

    Merge pull request #49 from OrderingPlus/avoid-index-caching-service-worker
    [FEAT] avoid-index-caching-service-worker

--- src/serviceWorkerRegistration.js
+++ src/serviceWorkerRegistration.js
@@ -126,13 +126,8 @@ function checkValidServiceWorker (swUrl, config) {
 
 export function unregister () {
   if ('serviceWorker' in navigator) {
-    window.addEventListener('load', () => {
-      navigator.serviceWorker.register('./service-worker.js')
-        .then(registration => {
-          console.log('SW registered: ', registration)
-        }).catch(registrationError => {
-          console.log('SW registration failed: ', registrationError)
-        })
+    navigator.serviceWorker.ready.then((registration) => {
+      registration.unregister()
     })
   }
 }
