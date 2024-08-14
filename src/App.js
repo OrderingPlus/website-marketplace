e49554daf23ea75058f3b0663942dbdd63699d3b
Jesus David <jesusdpch00@gmail.com>
2024-08-14 16:42:53 -0400

    chore: Improve OneSignal initialization and notification handling

--- src/App.js
+++ src/App.js
@@ -454,9 +454,10 @@ export const App = () => {
 
   const oneSignalSetup = () => {
     const OneSignal = window.OneSignal || []
+    const hostname = window.location.hostname
     const initConfig = {
       appId: configs?.onesignal_orderingweb_id?.value,
-      // allowLocalhostAsSecureOrigin: true,
+      allowLocalhostAsSecureOrigin: hostname === 'localhost',
       notificationClickHandlerAction: 'navigate'
     }
 
@@ -464,7 +465,9 @@ export const App = () => {
       OneSignal.SERVICE_WORKER_PARAM = { scope: '/push/onesignal/' }
       OneSignal.SERVICE_WORKER_PATH = 'push/onesignal/OneSignalSDKWorker.js'
       OneSignal.SERVICE_WORKER_UPDATER_PATH = 'push/onesignal/OneSignalSDKWorker.js'
-      OneSignal.init(initConfig)
+      if (!OneSignal._initCalled) {
+        OneSignal.init(initConfig)
+      }
 
       const onNotificationClicked = function (data) {
         if (data?.data?.order_uuid) {
@@ -473,9 +476,13 @@ export const App = () => {
       }
       const handler = function (data) {
         onNotificationClicked(data)
+        if (typeof OneSignal.addListenerForNotificationOpened === 'function') {
+          OneSignal.addListenerForNotificationOpened(handler)
+        }
+      }
+      if (typeof OneSignal.addListenerForNotificationOpened === 'function') {
         OneSignal.addListenerForNotificationOpened(handler)
       }
-      OneSignal.addListenerForNotificationOpened(handler)
 
       OneSignal.on('subscriptionChange', function (isSubscribed) {
         if (isSubscribed) {
